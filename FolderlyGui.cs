2c7cab7cc6e1232b0d093798f9eacb08a71fc478
Jannis Westphal <jannis.westphal@gmail.com>
2026-03-30 17:20:24 +0200

    setup minimal extension

---
+++ FolderlyGui.cs
@@ -0,0 +1,28 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+using static DevToys.Api.GUI;
+
+namespace Folderly;
+
+[Export(typeof(IGuiTool))]
+[Name("Folderly")]
+[ToolDisplayInformation(
+    IconFontName = "FluentSystemIcons",
+    IconGlyph = '\uE670',
+    GroupName = PredefinedCommonToolGroupNames.Converters,
+    ResourceManagerAssemblyIdentifier = nameof(FolderlyResourceAssemblyIdentifier),
+    ResourceManagerBaseName = "Folderly.Folderly",
+    ShortDisplayTitleResourceName = nameof(Folderly.ShortDisplayTitle),
+    LongDisplayTitleResourceName = nameof(Folderly.LongDisplayTitle),
+    DescriptionResourceName = nameof(Folderly.Description),
+    AccessibleNameResourceName = nameof(Folderly.AccessibleName))]
+public class FolderlyGui
+{
+    public UIToolView View =>
+        new(Label().Style(UILabelStyle.BodyStrong).Text("Hello World!"));
+
+    public void OnDataReceived(string dataTypeName, object? parsedData)
+    {
+        // throw new NotImplementedException();
+    }
+}
---
+++ FolderlyResourceAssemblyIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using DevToys.Api;
+using System.ComponentModel.Composition;
+
+namespace Folderly;
+
+[Export(typeof(IResourceAssemblyIdentifier))]
+[Name(nameof(FolderlyResourceAssemblyIdentifier))]
+internal sealed class FolderlyResourceAssemblyIdentifier : IResourceAssemblyIdentifier
+{
+    public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
